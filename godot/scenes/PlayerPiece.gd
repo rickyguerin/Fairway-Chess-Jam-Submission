3ed33d56e924ef852311289921ffbe3c70ebdc66
Ricky Guerin <Guerin.Ricky@gmail.com>
2024-03-01 13:17:15 -0600

    Apply hover material on mouse hover

--- godot/scenes/PlayerPiece.gd
+++ godot/scenes/PlayerPiece.gd
@@ -1,12 +1,14 @@
 extends Node3D
 
+@onready var HOVER_MATERIAL = preload("res://assets/materials/piece_hover.tres")
+
 
 func _ready():
 	$pawn/StaticBody3D.connect("mouse_entered", _on_mouse_entered)
 	$pawn/StaticBody3D.connect("mouse_exited", _on_mouse_exited)
 
 func _on_mouse_entered():
-	print("ENTER " + get_name())
+	$pawn.set_surface_override_material(0, HOVER_MATERIAL)
 
 func _on_mouse_exited():
-	print("EXIT " + get_name())
+	$pawn.set_surface_override_material(0, null)

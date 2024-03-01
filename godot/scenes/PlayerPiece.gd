19148e08df059b75ffcc75741e409aebd6e84ec7
Ricky Guerin <Guerin.Ricky@gmail.com>
2024-03-01 16:16:33 -0600

    Allow the piece to turn left and right

--- godot/scenes/PlayerPiece.gd
+++ godot/scenes/PlayerPiece.gd
@@ -7,26 +7,46 @@ const SELECTED_MATERIAL := preload("res://assets/materials/selected.tres")
 const ARROW_SCENE := preload("res://scenes/arrow.tscn")
 
 @onready var mouse_is_hovering := false
+@onready var is_selected := false
 
 func _ready():
 	$pawn/StaticBody3D.connect("mouse_entered", _on_mouse_entered)
 	$pawn/StaticBody3D.connect("mouse_exited", _on_mouse_exited)
+
+
+func _physics_process(delta):
+	if not is_selected:
+		return
+
+	if Input.is_action_pressed("A"):
+		rotate_y(0.05)
+
+	elif Input.is_action_pressed("D"):
+		rotate_y(-0.05)
+
 
 func _unhandled_input(event):
 	if mouse_is_hovering and event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 			clicked.emit(self)
 
+
 func select():
 	$pawn.set_surface_override_material(0, SELECTED_MATERIAL)
 	$arrow.visible = true
+	is_selected = true
+
 
 func unselect():
+	rotation = Vector3()
 	$pawn.set_surface_override_material(0, null)
 	$arrow.visible = false
+	is_selected = false
+
 
 func _on_mouse_entered():
 	mouse_is_hovering = true
+
 
 func _on_mouse_exited():
 	mouse_is_hovering = false

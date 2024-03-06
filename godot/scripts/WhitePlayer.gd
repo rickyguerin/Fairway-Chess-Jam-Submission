5685c17308d497da75623a44df015c34d065da89
Ricky Guerin <Guerin.Ricky@gmail.com>
2024-03-06 12:19:09 -0600

    Move logic from Game to WhitePlayer

--- godot/scripts/WhitePlayer.gd
+++ godot/scripts/WhitePlayer.gd
@@ -1,11 +1,37 @@
 extends Node
 
+@onready var can_act := false
+
+var selected_piece: PlayerPiece
 
 func _ready():
 	get_tree().root.get_child(0).connect("turn_start", _on_turn_start)
+	for node in get_tree().get_nodes_in_group("WhitePieces"):
+		node.connect("clicked", _select_piece)
+		node.connect("moved", _on_moved)
 
 
 func _on_turn_start(player: G.Player):
 	if not player == G.Player.WHITE:
 		return
-	print("WHITE")
+
+	can_act = true
+
+
+func _select_piece(node):
+	if node is PlayerPiece and node != selected_piece and can_act:
+		_unselect_piece()
+		node.select()
+		selected_piece = node
+
+
+func _unselect_piece():
+	if selected_piece:
+		selected_piece.unselect()
+
+	selected_piece = null
+
+
+func _on_moved():
+	can_act = false
+	_unselect_piece()
--- godot/scripts/game.gd
+++ godot/scripts/game.gd
@@ -2,32 +2,5 @@ extends Node
 
 signal turn_start(player)
 
-var selected_piece: Node3D
-var can_act := true
-
 func _ready():
-	for child in $"White Pieces".get_children():
-		child.connect("clicked", _select_piece)
-		child.connect("capture", _unselect_all)
-		child.connect("moved", _deactivate_pieces)
-
 	turn_start.emit(G.Player.WHITE)
-
-
-func _unselect_all():
-	for child in $"White Pieces".get_children():
-		(child as PlayerPiece).unselect()
-
-	selected_piece = null
-
-
-func _select_piece(node):
-	if node is PlayerPiece and node != selected_piece and can_act:
-		_unselect_all()
-		node.select()
-		selected_piece = node
-
-
-func _deactivate_pieces():
-	can_act = false
-	_unselect_all()

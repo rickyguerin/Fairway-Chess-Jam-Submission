887d331c37645b269eea2740d7fde51b2e9921f5
Ricky Guerin <Guerin.Ricky@gmail.com>
2024-03-07 14:10:11 -0600

    Create basic power meter UI

--- godot/scripts/game.gd
+++ godot/scripts/game.gd
@@ -4,8 +4,19 @@ signal turn_start(player)
 
 func _ready():
 	$WhitePlayer.connect("turn_end", _on_turn_end)
+	$WhitePlayer.connect("select", _on_piece_select)
+	$WhitePlayer.connect("unselect", _on_piece_unselect)
+	
 	$BlackPlayer.connect("turn_end", _on_turn_end)
 	turn_start.emit(G.Player.WHITE)
+
+
+func _on_piece_select():
+	$PowerMeterUI.visible = true
+
+
+func _on_piece_unselect():
+	$PowerMeterUI.visible = false
 
 
 func _on_turn_end(player: G.Player):

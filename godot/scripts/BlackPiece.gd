956dee9867c226721a23779d2a8ca7fe56122058
Ricky Guerin <Guerin.Ricky@gmail.com>
2024-03-06 21:36:38 -0600

    rename is_selected to can_rotate

--- godot/scripts/BlackPiece.gd
+++ godot/scripts/BlackPiece.gd
@@ -10,7 +10,7 @@ const SELECTED_MATERIAL := preload("res://assets/materials/selected.tres")
 @export var impulse_direction := Vector3(0, 0, -1)
 @export var allowed_directions: Array[float] = [0, -45, -90, -135, 180, 135, 90, 45]
 
-@onready var is_selected := false
+@onready var can_rotate := false
 
 
 func _ready():
@@ -18,13 +18,13 @@ func _ready():
 
 
 func _integrate_forces(state):
-	if not is_selected:
+	if not can_rotate:
 		return
 
-	is_selected = false
+	can_rotate = false
 
 	var trans = state.get_transform()
-	trans.basis = trans.basis.from_euler(Vector3(0, deg_to_rad(allowed_directions[0]), 0))
+	trans.basis = trans.basis.from_euler(Vector3(0, deg_to_rad(allowed_directions.pick_random()), 0))
 	state.set_transform(trans)
 
 

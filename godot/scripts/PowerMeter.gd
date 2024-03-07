66e7fb5e1ae873f3bf1937ee8d78f424dd43992a
Ricky Guerin <Guerin.Ricky@gmail.com>
2024-03-07 17:02:59 -0600

    Only try connect signal if player has signal

--- godot/scripts/PowerMeter.gd
+++ godot/scripts/PowerMeter.gd
@@ -1,11 +1,15 @@
 extends ProgressBar
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	for player in get_tree().get_nodes_in_group("Players"):
-		player.connect("start_swing", _on_start_swing)
+		if player.has_signal("start_swing"):
+			player.connect("start_swing", _on_start_swing)
 
 
-func _on_start_swing():
+func _process(delta):
+	pass
+
+
+func _on_start_swing(player: G.Player):
 	pass
